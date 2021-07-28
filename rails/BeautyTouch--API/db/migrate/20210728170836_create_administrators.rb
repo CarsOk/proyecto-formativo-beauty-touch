9d4f04baf32f6dc0b8743ca630aba717e7fc8afb
Teddys Cortes <tediscortez22@gmail.com>
2021-07-28 12:56:22 -0500

    eh creado las tablas Cliente, Administrador y empleados. porfavor verificar que datos hacen falta y que este todo en orden

---
+++ rails/BeautyTouch--API/db/migrate/20210728170836_create_administrators.rb
@@ -0,0 +1,18 @@
+class CreateAdministrators < ActiveRecord::Migration[6.1]
+  def change
+    create_table :administrators do |t|
+      t.int :Id_Adm
+      t.string :Name1_Adm
+      t.string :Name2_Adm
+      t.string :LastName1_Adm
+      t.string :LastName2_Adm
+      t.date :Birthay_Adm
+      t.int :Cellphone_Adm
+      t.string :Email_Adm
+      t.string :Password_Adm
+      t.string :direction_Emp
+
+      t.timestamps
+    end
+  end
+end
---
+++ rails/BeautyTouch--API/db/migrate/20210728174614_create_employees.rb
@@ -0,0 +1,19 @@
+class CreateEmployees < ActiveRecord::Migration[6.1]
+  def change
+    create_table :employees do |t|
+      t.int :Id_Emp
+      t.string :Name1_Emp
+      t.string :Name2_Emp
+      t.string :LastName1_Emp
+      t.string :LastName2_Emp
+      t.date :Birthay_Emp
+      t.int :Cellphone_Emp
+      t.string :Email_Emp
+      t.string :Password_Emp
+      t.string :WorkArea_Emp
+      t.string :direction_Emp
+
+      t.timestamps
+    end
+  end
+end
